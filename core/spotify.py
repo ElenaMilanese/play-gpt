66a5ecd6f51ab59edeee0c07b3e31fd058f72aee
elenm <elena.milanese91@gmail.com>
2023-10-06 15:38:48 -0300

    [feat] Requirements

--- core/spotify.py
+++ core/spotify.py
@@ -2,6 +2,7 @@ from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from config import SpotifyConfig
 from utils import process_string, erase_diacritics, erase_punctuation
+from typing import Dict, List
 
 
 
@@ -35,22 +36,24 @@ class SpotifyPlaylist:
             )
         )
         
+    def _search_songs(songs:Dict[str, Dict[str, str]]) -> List:
+        """Searches for the songs in Spotify"""
+        songs_list = []
+        for song in songs["canciones"]:
+            try:
+                norm_name = process_string(song["nombre"], [erase_diacritics, erase_punctuation])
+                norm_artist = remove_punctuation(remove_diacritics(song["artista"]))
+                result = sp.search(q=f'track:{norm_name} artist:{norm_artist}', type='track')
+                uri = result['tracks']['items'][0]['uri']
+                songs.append(uri)
+            except:
+                print(f'La canción {song["nombre"]} de {song["artista"]} no existe en spotify, quizás flashó gpt :P')
+        songs_unique = list(set(songs))
 
-    
-songs = []
-for song in playlist["canciones"]:
-    try:
-        norm_name = process_string(song["nombre"], [erase_diacritics, erase_punctuation])
-        norm_artist = remove_punctuation(remove_diacritics(song["artista"]))
-        result = sp.search(q=f'track:{norm_name} artist:{norm_artist}', type='track')
-        uri = result['tracks']['items'][0]['uri']
-        songs.append(uri)
-    except:
-        print(f'La canción {song["nombre"]} de {song["artista"]} no existe en spotify, quizás flashó gpt :P')
-songs_unique = list(set(songs))
 
-user_id = sp.current_user()["id"]
-playlist = sp.user_playlist_create(user=user_id, name=nombre, public=False)
+    def create_playlist():
+        user_id = sp.current_user()["id"]
+        playlist = sp.user_playlist_create(user=user_id, name=nombre, public=False)
 
-sp.playlist_add_items(playlist_id=playlist["id"], items=songs_unique)
-print(f"Se creó la lista de reproducción {nombre}.")
+        sp.playlist_add_items(playlist_id=playlist["id"], items=songs_unique)
+        print(f"Se creó la lista de reproducción {nombre}.")
